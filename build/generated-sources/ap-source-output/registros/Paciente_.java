5fa94781ca303babbb336f2ed674a7545751d409
Enzo Vignotti <enzovignottisabino@gmail.com>
2024-09-13 06:29:32 -0300

    Poucas coisas

---
+++ build/generated-sources/ap-source-output/registros/Paciente_.java
@@ -0,0 +1,26 @@
+package registros;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-09-13T06:22:55", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Paciente.class)
+public class Paciente_ { 
+
+    public static volatile SingularAttribute<Paciente, String> endereco;
+    public static volatile SingularAttribute<Paciente, String> info_contatoEmail;
+    public static volatile SingularAttribute<Paciente, String> bebe;
+    public static volatile SingularAttribute<Paciente, String> nome;
+    public static volatile SingularAttribute<Paciente, Integer> info_contatoCelular;
+    public static volatile SingularAttribute<Paciente, String> cirurgias;
+    public static volatile SingularAttribute<Paciente, String> nivelColesterol;
+    public static volatile SingularAttribute<Paciente, String> alergias;
+    public static volatile SingularAttribute<Paciente, String> diabete;
+    public static volatile SingularAttribute<Paciente, String> doencaCardiaca;
+    public static volatile SingularAttribute<Paciente, String> fuma;
+    public static volatile SingularAttribute<Paciente, String> data_nascimento;
+    public static volatile SingularAttribute<Paciente, Integer> id;
+    public static volatile SingularAttribute<Paciente, String> tipo_convenio;
+
+}
